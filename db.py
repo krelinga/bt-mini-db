dc2f4cece65a128840c0880dc1db77a7b8f4bb47
Andy Kreling <krelinga@gmail.com>
2019-08-24 16:33:56 -0500

    play around with AS units and templates based on meta-programming

--- db.py
+++ db.py
@@ -100,3 +100,93 @@ minis = MiniatureDB()
 print(minis.mad_cat_0)
 print(minis.mad_cat_1)
 print(minis.vulture_0)
+
+
+def _FormatOrNone(fmt, value):
+    if value is None:
+        return None
+    return fmt % value
+
+
+class AlphaStrikeUnitTemplate(object):
+    def __init__(self, template=None, name=None, model=None, pv=None, tp=None,
+                 sz=None, mv=None, mvj=None, role=None, ds=None, dm=None,
+                 dl=None, de=None, ov=None, a=None, s=None, specials=None):
+        self._template = template
+
+        self._name = name #if name is not None else None
+        self._model = model #if model is not None else None
+        #if name is not None:
+        #    self._name = name
+#        if model is not None:
+#            self.model = model
+#        if pv is not None:
+#            self.pv = pv
+#        if tp is not None:
+#            self.tp = tp
+#        if sz is not None:
+#            self.sz = sz
+#        if mv is not None:
+#            self.mv = mv
+#        if mvj is not None:
+#            self.mvj = mvj
+#        if role is not None:
+#            self.role = role
+#        if ds is not None:
+#            self.ds = ds
+#        if dm is not None:
+#            self.dm = dm
+#        if dl is not None:
+#            self.dl = dl
+#        if de is not None:
+#            self.de = de
+#        if ov is not None:
+#            self.ov = ov
+#        if a is not None:
+#            self.a = a
+#        if s is not None:
+#            self.s = s
+#        if specials is not None:
+#            self.specials = specials
+
+    def _resolve(self, prop):
+        self_val = self.__dict__.get(prop)
+        if self_val is not None:
+            return self_val
+        if self._template is None:
+            return None
+        return self._template.__dict__.get(prop)
+
+    @property
+    def name(self):
+        return self._resolve('_name')
+
+    @property
+    def model(self):
+        return self._resolve('_model')
+
+    def __str__(self):
+        parts = [
+            ('name', '"%s"'),
+            ('model', '"%s"'),
+        ]
+        parts_strs = ['%s: %s' % (x[0], _FormatOrNone(x[1], self._resolve('_%s' % x[0]))) for x in parts]
+        return "{ %s }" % '\t'.join(parts_strs)
+
+
+class AlphaStrikeUnit(AlphaStrikeUnitTemplate):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        if self.name is None:
+            raise Error('name must be specified')
+        if self.model is None:
+            raise Error('model must be specified')
+
+
+mad_cat_tmpl = AlphaStrikeUnitTemplate(name="Mad Cat")
+print('%s' % mad_cat_tmpl)
+mad_cat_unit = AlphaStrikeUnit(template=mad_cat_tmpl, model='Prime')
+print('%s' % mad_cat_unit)
+mad_cat_unit2 = AlphaStrikeUnit(template=mad_cat_tmpl)
+print('%s' % mad_cat_unit2)
