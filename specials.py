6753a6a4a7c63789357343e3f9392b12153c6454
Andy Kreling <krelinga@gmail.com>
2019-08-29 23:26:23 -0500

    add a bunch of mad cat variants

--- specials.py
+++ specials.py
@@ -1,3 +1,12 @@
+def _FormatNumber(x):
+    if x is None:
+        return '-'
+    return '%d' % x
+
+
+AMS = 'AMS'
+
+
 CASE = 'CASE'
 
 
@@ -6,7 +15,21 @@ def IF(num):
 
 
 def LRM(s, m, l):
-    return 'LRM%d/%d/%d' % (s, m, l)
+    return 'LRM%s/%s/%s' % (_FormatNumber(s), _FormatNumber(m), _FormatNumber(l))
+
+
+LTAG = 'LTAG'
 
 
 OMNI = 'OMNI'
+
+
+OVL = 'OVL'
+
+
+def REAR(s, m, l):
+    return 'REAR%s/%s/%s' % (_FormatNumber(s), _FormatNumber(m), _FormatNumber(l))
+
+
+def SRM(s, m):
+    return 'SRM%s/%s' % (_FormatNumber(s), _FormatNumber(m))
